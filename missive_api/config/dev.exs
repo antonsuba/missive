f3ba81de57135086cfcbc382b65c04b1301fb6e7
antonsuba <antonsuba@outlook.com>
2023-12-03 19:13:30 +0800

    Setup database url config

--- missive_api/config/dev.exs
+++ missive_api/config/dev.exs
@@ -2,10 +2,6 @@ import Config
 
 # Configure your database
 config :missive_api, MissiveApi.Repo,
-  username: "postgres",
-  password: "postgres",
-  hostname: "localhost",
-  database: "missive_api_dev",
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
